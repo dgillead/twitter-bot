af0651e95276cb4c1a984db6d39fa217cf192762
Darnell Gillead <dlgillead@gmail.com>
2017-05-04 20:33:06 -0700

    add random and favoriteTweet functions

--- bot.js
+++ bot.js
@@ -31,3 +31,34 @@ var retweet = function() {
 
 retweet();
 setInterval(retweet, 3000000);
+
+var favoriteTweet = function() {
+  var params = {
+    q: '#nodejs, #Nodejs',
+    result_type: 'recent',
+    lang: 'en'
+  }
+
+  Twitter.get('search/tweets', params, function(err, data) {
+    var tweet = data.statuses;
+    var randomTweet = ranDom(tweet);
+    if (typeof randomTweet != 'undefined') {
+      Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response) {
+        if (err) {
+          console.log('CANNOT BE FAVORITE, ERROR');
+        }
+        else {
+          console.log('FAVORITED');
+        }
+      });
+    }
+  });
+}
+
+favoriteTweet();
+setInterval(favoriteTweet, 3600000);
+
+function ranDom (arr) {
+  var index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+};
