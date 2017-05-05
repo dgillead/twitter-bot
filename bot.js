3e54e0f71d263e890b365b805c104c26a1489f5b
Darnell Gillead <dlgillead@gmail.com>
2017-05-04 20:27:00 -0700

    add retweet function

--- bot.js
+++ bot.js
@@ -1,3 +1,33 @@
 var twit = require('twit');
 var config = require('./config.js')
 var Twitter = new twit(config);
+
+var retweet = function() {
+  var params = {
+    q: '#nodejs, #Nodejs',
+    result_type: 'recent',
+    lang: 'en'
+  }
+
+  Twitter.get('search/tweets', params, function(err, data) {
+    if (!err) {
+      var retweetID = data.statuses[0].id_str;
+      Twitter.post('statuses/retweet/:id'), {
+        id: retweetID
+      }, function(err, response) {
+        if (response) {
+          console.log('Retweeted!!!');
+        }
+        if (err) {
+          console.log('Something went wrong while RETWEETING');
+        }
+      });
+    }
+    else {
+      console.log('Something went wrong while SEARCHING')
+    }
+  });
+}
+
+retweet();
+setInterval(retweet, 3000000);
