d272184fc435239a660a094e83f0b70bddf62f2c
Darnell Gillead <dlgillead@gmail.com>
2017-05-08 17:47:13 -0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'cKIDp8fbMuCXAvlHBvMciOkfI',
-  consumer_secret: 'UkKTOuOtctH4odFV6Z6WWzrhSC79FpU2jjVQkdDakf7ghmvWna',
-  access_token: '3047658283-zbXkAU9MqPRrmad1LonbwYdTkAq9CTD722co0uJ',
-  access_token_secret: 'iGZz33UbT5XRF8UrFLhrH5l7oLFb4QpcFCpSiVrRgsGV4'
-}
